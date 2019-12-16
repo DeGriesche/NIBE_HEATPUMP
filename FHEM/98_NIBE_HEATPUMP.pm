21b93cb655cce3b502d0b75bc3dae1f04d30cd9a
DeGriesche <mano.pavlakis@gmail.com>
2019-12-16 11:59:31 +0100

    Add files via upload

--- FHEM/98_NIBE_HEATPUMP.pm
+++ FHEM/98_NIBE_HEATPUMP.pm
@@ -31,24 +31,24 @@ my %parameter = (
 );
 
 sub NIBE_HEATPUMP_Initialize($) {
-    my ($hash) = @_;
+	my ($hash) = @_;
 
-    $hash->{DefFn}      = 'NIBE_HEATPUMP_Define';
-    $hash->{UndefFn}    = 'NIBE_HEATPUMP_Undef';
-    $hash->{SetFn}      = 'NIBE_HEATPUMP_Set';
-    $hash->{AttrFn}     = 'NIBE_HEATPUMP_Attr';
+	$hash->{DefFn}      = 'NIBE_HEATPUMP_Define';
+	$hash->{UndefFn}    = 'NIBE_HEATPUMP_Undef';
+	$hash->{SetFn}      = 'NIBE_HEATPUMP_Set';
+	$hash->{AttrFn}     = 'NIBE_HEATPUMP_Attr';
 
-    $hash->{AttrList} = "systemId refreshInterval debugMode:0,1 maxNotifications ".$readingFnAttributes;
+	$hash->{AttrList} = "systemId refreshInterval debugMode:0,1 maxNotifications ".$readingFnAttributes;
 }
 
 sub NIBE_HEATPUMP_Define($$) {
-    my ($hash, $def) = @_;
-    my @param = split('[ \t]+', $def);
-    
+	my ($hash, $def) = @_;
+	my @param = split('[ \t]+', $def);
+
 	if(int(@param) != 4) {
-    	return "wrong number of parameters: define <name> NIBE_HEATPUMP <clientId> <clientSecret>";
-    }
-	
+		return "wrong number of parameters: define <name> NIBE_HEATPUMP <clientId> <clientSecret>";
+	}
+
 	$hash->{name}  = $param[0];
 	$hash->{clientId} = $param[2];
 	$hash->{clientSecret} = $param[3];    
@@ -59,7 +59,7 @@ sub NIBE_HEATPUMP_Define($$) {
 	$attr{$hash->{NAME}}{devStateIcon} = "DEFAULT_OPERATION:rc_HOME AWAY_FROM_HOME:user_away VACATION:user_ext_away";
 	$attr{$hash->{NAME}}{icon} = "sani_buffer_temp_all";
 
-    return undef;
+	return undef;
 }
 
 sub NIBE_HEATPUMP_Undef($$) {
@@ -76,7 +76,7 @@ sub NIBE_HEATPUMP_Delete ($$) {
 }
 
 sub NIBE_HEATPUMP_Set($@) {
-	my ( $hash, $name, $cmd, @args ) = @_;
+	my ($hash, $name, $cmd, @args) = @_;
 	
 	return "\"set $name\" needs at least one argument" unless(defined($cmd));
 
@@ -111,17 +111,17 @@ sub NIBE_HEATPUMP_Set($@) {
 sub NIBE_HEATPUMP_Attr(@) {
 	my ($cmd,$name,$attr_name,$attr_value) = @_;
 	if($cmd eq "set") {
-        if($attr_name eq "systemId") {
+		if($attr_name eq "systemId") {
 			if(length($attr_value) ne 5 || !looks_like_number($attr_value)) {
-			    my $err = "Invalid argument $attr_value to $attr_name. Must be numeric with 5 digits";
-			    Log3 $name, 3, $err;
-			    return3 $err;
+				my $err = "Invalid argument $attr_value to $attr_name. Must be numeric with 5 digits";
+				Log3 $name, 3, $err;
+				return3 $err;
 			}
 		} elsif ($attr_name =~ m/(refreshInterval|maxNotifications)/) {
 			if(!looks_like_number($attr_value)) {
-			    my $err = "Invalid argument $attr_value to $attr_name. Must be numeric";
-			    Log3 $name, 3, $err;
-			    return $err;
+				my $err = "Invalid argument $attr_value to $attr_name. Must be numeric";
+				Log3 $name, 3, $err;
+				return $err;
 			}
 		} 	
 	}
@@ -131,22 +131,22 @@ sub NIBE_HEATPUMP_Attr(@) {
 sub NIBE_HEATPUMP_getToken($) {
 	my ($hash) = @_;
 	my $name = $hash->{NAME};
-	
+
 	if (ReadingsVal($name, ".token_expiration", 0) <= time()) {
 		NIBE_HEATPUMP_refreshToken($hash);
 	}
-	
+
 	return ReadingsVal($name, ".access_token", 0);
 }
 
 sub NIBE_HEATPUMP_saveToken($$$) {
 	my ($hash, $err, $data) = @_;
-    my $name = $hash->{NAME};
+	my $name = $hash->{NAME};
 	Log3 $name, 1, "Saving Token" if ($attr{$name}{debugMode});
 
-    if ($err ne "") {
-        Log3 $name, 3, "error while requesting - $err";
-    } elsif ($data ne "") {
+	if ($err ne "") {
+		Log3 $name, 3, "error while requesting - $err";
+	} elsif ($data ne "") {
 		my $decoded = decode_json($data);
 		if (exists $decoded->{error}) {
 			Log3 $name, 3, "error while requesting - ".$decoded->{error};
@@ -158,14 +158,14 @@ sub NIBE_HEATPUMP_saveToken($$$) {
 			readingsEndUpdate($hash, 1);
 			Log3 $name, 1, "Got new Token" if ($attr{$name}{debugMode});
 		}
-    }
+	}
 }
 
 sub NIBE_HEATPUMP_requestToken($$) {
 	my ($hash, $accessCode) = @_;
 	my $name = $hash->{NAME};
 	Log3 $name, 1, "Requesting Token" if ($attr{$name}{debugMode});
-	
+
 	chomp $accessCode;
 	my %urlParams = (
 		"grant_type" => "authorization_code",
@@ -175,7 +175,7 @@ sub NIBE_HEATPUMP_requestToken($$) {
 		"redirect_uri" => $redirectUrl,
 		"scope" => "READSYSTEM+WRITESYSTEM"
 	);
-		
+	
 	my $param = {
 		url => $oauthTokenBaseUrl,
 		timeout => 5,
@@ -192,7 +192,7 @@ sub NIBE_HEATPUMP_refreshToken($) {
 	my ($hash) = @_;
 	my $name = $hash->{NAME};
 	Log3 $name, 1, "Refreshing Token" if ($attr{$name}{debugMode});
-	
+
 	my %urlParams = (
 		"grant_type" => "refresh_token",
 		"client_id" => urlEncode($hash->{clientId}),
@@ -215,11 +215,11 @@ sub NIBE_HEATPUMP_refresh($) {
 	my ($hash) = @_;
 	my $name = $hash->{NAME};
 	Log3 $name, 1, "Refresh" if ($attr{$name}{debugMode});
-						
+					
 	my $nextRefresh = gettimeofday() + $attr{$name}{refreshInterval};
 	readingsSingleUpdate($hash, "nextRefresh", FmtDateTime($nextRefresh), 1);
 	InternalTimer($nextRefresh, "NIBE_HEATPUMP_refresh", $hash);
-	
+
 	NIBE_HEATPUMP_refreshSmartHomeMode($hash);
 	NIBE_HEATPUMP_refreshParameters($hash);
 	NIBE_HEATPUMP_refreshConfig($hash);
@@ -227,17 +227,17 @@ sub NIBE_HEATPUMP_refresh($) {
 }
 
 sub NIBE_HEATPUMP_refreshSmartHomeMode($) {
-    my ($hash) = @_;
+	my ($hash) = @_;
 	my $name = $hash->{NAME};
 	Log3 $name, 1, "Refresh SmartHome mode" if ($attr{$name}{debugMode});
 
 	my $param = {
-		url        => "$apiBaseUrl/systems/".$attr{$name}{systemId}."/smarthome/mode",
-		timeout    => $apiTimeout,
-		hash       => $hash, 
-		method     => "GET",
-		header     => "Authorization: Bearer ".NIBE_HEATPUMP_getToken($hash),
-		callback   => sub($) {
+		url => "$apiBaseUrl/systems/".$attr{$name}{systemId}."/smarthome/mode",
+		timeout => $apiTimeout,
+		hash => $hash, 
+		method => "GET",
+		header => "Authorization: Bearer ".NIBE_HEATPUMP_getToken($hash),
+		callback => sub($) {
 						my ($param, $err, $data) = @_;
   						my $hash = $param->{hash};
     					if ($err ne "") {
@@ -258,17 +258,17 @@ sub NIBE_HEATPUMP_refreshSmartHomeMode($) {
 }
 
 sub NIBE_HEATPUMP_refreshParameters($) {
-    my ($hash) = @_;
+	my ($hash) = @_;
 	my $name = $hash->{NAME};
 	Log3 $name, 1, "Refreshing parameter" if ($attr{$name}{debugMode});
-	
+
 	my $param = {
-		url        => "$apiBaseUrl/systems/".$attr{$name}{systemId}."/parameters?parameterIds=".join("&parameterIds=", keys %parameter),
-		timeout    => $apiTimeout,
-		hash       => $hash, 
-		method     => "GET",
-		header     => "Authorization: Bearer ".NIBE_HEATPUMP_getToken($hash),
-		callback   => sub($) {
+		url => "$apiBaseUrl/systems/".$attr{$name}{systemId}."/parameters?parameterIds=".join("&parameterIds=", keys %parameter),
+		timeout => $apiTimeout,
+		hash => $hash, 
+		method => "GET",
+		header => "Authorization: Bearer ".NIBE_HEATPUMP_getToken($hash),
+		callback => sub($) {
 						my ($param, $err, $data) = @_;
   						my $hash = $param->{hash};
     					if ($err ne "") {
@@ -300,14 +300,14 @@ sub NIBE_HEATPUMP_refreshConfig($) {
 	my ($hash) = @_;
 	my $name = $hash->{NAME};
 	Log3 $name, 1, "Refreshing config" if ($attr{$name}{debugMode});
-	
+
 	my $param = {
-		url        => "$apiBaseUrl/systems/".$attr{$name}{systemId}."/config",
-		timeout    => $apiTimeout,
-		hash       => $hash, 
-		method     => "GET",
-		header     => "Authorization: Bearer ".NIBE_HEATPUMP_getToken($hash),
-		callback   => sub($) {
+		url => "$apiBaseUrl/systems/".$attr{$name}{systemId}."/config",
+		timeout => $apiTimeout,
+		hash => $hash, 
+		method => "GET",
+		header => "Authorization: Bearer ".NIBE_HEATPUMP_getToken($hash),
+		callback => sub($) {
 						my ($param, $err, $data) = @_;
   						my $hash = $param->{hash};
     					if ($err ne "") {
@@ -335,7 +335,7 @@ sub NIBE_HEATPUMP_refreshNotifications($) {
 	my ($hash) = @_;
 	my $name = $hash->{NAME};
 	Log3 $name, 1, "Refreshing notifications" if ($attr{$name}{debugMode});
-	
+
 	my $param = {
 		url        => "$apiBaseUrl/systems/".$attr{$name}{systemId}."/notifications?itemsPerPage=".$attr{$name}{maxNotifications},
 		timeout    => $apiTimeout,
@@ -377,10 +377,10 @@ sub NIBE_HEATPUMP_refreshNotifications($) {
 }
 
 sub NIBE_HEATPUMP_setSmartHomeMode($$) {
-    my ($hash, $value) = @_;
+	my ($hash, $value) = @_;
 	my $name = $hash->{NAME};
 	Log3 $name, 1, "Set SmartHome mode $value" if ($attr{$name}{debugMode});
-	
+
 	my $param = {
 		url => "$apiBaseUrl/systems/".$attr{$name}{systemId}."/smarthome/mode",
 		timeout => 10,
@@ -389,10 +389,10 @@ sub NIBE_HEATPUMP_setSmartHomeMode($$) {
 		header => "Authorization: Bearer ".NIBE_HEATPUMP_getToken($hash)."\nContent-Type: application/json"
 	};
 	my ($err, $data) = HttpUtils_BlockingGet($param);
-	
+
 	if ($err ne "") {
 		Log3 $hash->{NAME}, 3, "error while requesting ".$param->{url}." - $err";
-    } else {
+	} else {
 		NIBE_HEATPUMP_refresh($hash);
 	}
 }
@@ -400,76 +400,68 @@ sub NIBE_HEATPUMP_setSmartHomeMode($$) {
 1;
 
 =pod
+=item device
+
 =begin html
 
 <a name="NIBE_HEATPUMP"></a>
 <h3>NIBE_HEATPUMP</h3>
 <ul>
-    <i>NIBE_HEATPUMP</i> implements the classical "Hello World" as a starting point for module development. 
-    You may want to copy 98_Hello.pm to start implementing a module of your very own. See 
-    <a href="http://wiki.fhem.de/wiki/DevelopmentModuleIntro">DevelopmentModuleIntro</a> for an 
-    in-depth instruction to your first module.
+    <i>NIBE_HEATPUMP</i> implements a binding to <a href="https://api.nibeuplink.com/">NIBE Uplink API</a>.
     <br><br>
-    <a name="Hellodefine"></a>
+    <a name="NIBE_HEATPUMP_Define"></a>
     <b>Define</b>
     <ul>
-        <code>define &lt;name&gt; Hello &lt;greet&gt;</code>
+        <code>define &lt;name&gt; NIBE_HEATPUMP &lt;clientId&gt; &lt;clientSecret&gt;</code><br><br>
+		&lt;clientId&gt; specifies the <i>Identifier</i> from your NIBE Uplink application<br>
+		&lt;clientSecret&gt; specifies the <i>Secret</i> from your NIBE Uplink application
         <br><br>
-        Example: <code>define HELLO Hello TurnUrRadioOn</code>
-        <br><br>
-        The "greet" parameter has no further meaning, it just demonstrates
-        how to set a so called "Internal" value. See <a href="http://fhem.de/commandref.html#define">commandref#define</a> 
-        for more info about the define command.
+        Example: <br>
+		<code>define MyHeatpump NIBE_HEATPUMP 8s23vf8sw2s fh7823hklgf3490sfl3455mjnj54mvuz4s78</code>
     </ul>
     <br>
     
-    <a name="Helloset"></a>
+    <a name="NIBE_HEATPUMP_Set"></a>
     <b>Set</b><br>
     <ul>
         <code>set &lt;name&gt; &lt;option&gt; &lt;value&gt;</code>
         <br><br>
-        You can <i>set</i> any value to any of the following options. They're just there to 
-        <i>get</i> them. See <a href="http://fhem.de/commandref.html#set">commandref#set</a> 
-        for more info about the set command.
+        You can <i>set</i> any value to any of the following options.
         <br><br>
         Options:
         <ul>
-              <li><i>satisfaction</i><br>
-                  Defaults to "no"</li>
-              <li><i>whatyouwant</i><br>
-                  Defaults to "can't"</li>
-              <li><i>whatyouneed</i><br>
-                  Defaults to "try sometimes"</li>
+			<li><i>accessCode</i><br>
+				When initializing the module you need to pass a code generated by NIBE Uplink. You will get that code by visiting the website with URL from Reading <i>accessCodeUrl</i>.
+			</li><br>
+			<li><i>systemId</i><br>
+				When initializing the module you neet to pass the ID of your system. To get your ID visit <a href="https://www.nibeuplink.com/">NIBE Uplink</a> and select your system. Then you will see a URL like <i>https://www.nibeuplink.com/System/<b>&lt;systemID&gt;</b>/Status/Overview</i>.
+			</li><br>
+			<li><i>refresh</i><br>
+				Reload all Readings from NIBE Uplink. No arguments are required.
+			</li><br>
+			<li><i>mode</i><br>
+				Set the SmartHome mode of your heatpump. Possible values are <code>DEFAULT_OPERATION</code>, <code>AWAY_FROM_HOME</code> and <code>VACATION</code>.
+			</li><br>
         </ul>
     </ul>
     <br>
-
-    <a name="Helloget"></a>
-    <b>Get</b><br>
-    <ul>
-        <code>get &lt;name&gt; &lt;option&gt;</code>
-        <br><br>
-        You can <i>get</i> the value of any of the options described in 
-        <a href="#Helloset">paragraph "Set" above</a>. See 
-        <a href="http://fhem.de/commandref.html#get">commandref#get</a> for more info about 
-        the get command.
-    </ul>
-    <br>
     
-    <a name="Helloattr"></a>
+    <a name="NIBE_HEATPUMP_Attr"></a>
     <b>Attributes</b>
     <ul>
         <code>attr &lt;name&gt; &lt;attribute&gt; &lt;value&gt;</code>
         <br><br>
-        See <a href="http://fhem.de/commandref.html#attr">commandref#attr</a> for more info about 
-        the attr command.
-        <br><br>
         Attributes:
         <ul>
-            <li><i>formal</i> no|yes<br>
-                When you set formal to "yes", all output of <i>get</i> will be in a
-                more formal language. Default is "no".
-            </li>
+			<li><i>systemId</i> numeric<br>
+				When initializing the module you neet to pass the ID of your system. To get your ID visit <a href="https://www.nibeuplink.com/">NIBE Uplink</a> and select your system. Then you will see a URL like <i>https://www.nibeuplink.com/System/<b>&lt;systemID&gt;</b>/Status/Overview</i>.
+			</li><br>
+			<li><i>refreshInterval</i> seconds<br>
+				Seconds to next refresh of all Readings.
+			</li><br>
+			<li><i>maxNotifications</i> numeric<br>
+				Maximum number of notifications that will be requested from NIBE Uplink.
+			</li><br>
         </ul>
     </ul>
 </ul>
