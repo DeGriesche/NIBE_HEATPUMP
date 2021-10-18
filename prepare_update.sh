93c98308f5eb7130f0927f93f01d989a8249e903
DeGriesche <mano.pavlakis@gmail.com>
2021-10-18 20:13:13 +0200

    Create prepare_update.sh

---
+++ prepare_update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#compass compile --force
+
+rm controls_nibe_heatpump.txt
+
+echo "MOV ./www/hausautomatisierung-com/custom.js unused" >> controls_nibe_heatpump.txt
+
+find ./www -type f \( ! -iname ".*" \) -print0 | while IFS= read -r -d '' f;
+do
+    out="UPD "$(stat -f "%Sm" -t "%Y-%m-%d_%T" $f)" "$(stat -f%z $f)" ${f}"
+    echo ${out//.\//} >> controls_nibe_heatpump.txt
+done
+
+# CHANGED file
+echo "FHEM NIBE_HEATPUMP last changes:" > CHANGED
+echo $(date +"%Y-%m-%d") >> CHANGED
+git log -n 10 --reverse --pretty="format:- %s" >> CHANGED
