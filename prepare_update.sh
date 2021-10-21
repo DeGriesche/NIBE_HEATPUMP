084476060657ae7d18cafb6ea06e65f630193584
DeGriesche <mano.pavlakis@gmail.com>
2021-10-21 14:14:04 +0200

    Update prepare_update.sh

--- prepare_update.sh
+++ prepare_update.sh
@@ -8,7 +8,7 @@ do
     FILESIZE=$(stat -c%s "$FILE")
 	FILE=$(echo "$FILE"  | cut -c 3-)
 	printf "UPD %s %-7d %s\n" "$TIME" "$FILESIZE" "$FILE"  >> controls_nibe_heatpump.txt
-done <   <(find . -maxdepth 3 \( -name "*.pm" -o -name "*.txt" -o -name "*.svg" \) -print0 | sort -z -g)
+done <   <(find . \( -name "*.pm" -o -name "*.txt" -o -name "*.svg" \) -print0 | sort -z -g)
 
 # CHANGED file
 echo "FHEM NIBE_HEATPUMP last changes:" > CHANGED
