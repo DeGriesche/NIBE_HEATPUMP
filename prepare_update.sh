2650ed63ced8b1e7cb165c26bb81664efc8944a1
DeGriesche <mano.pavlakis@gmail.com>
2021-10-21 14:09:40 +0200

    Update prepare_update.sh

--- prepare_update.sh
+++ prepare_update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 rm controls_nibe_heatpump.txt
+
 while IFS= read -r -d '' FILE
 do
     TIME=$(git log --pretty=format:%cd -n 1 --date=iso -- "$FILE")
@@ -8,6 +9,15 @@ do
 	FILE=$(echo "$FILE"  | cut -c 3-)
 	printf "UPD %s %-7d %s\n" "$TIME" "$FILESIZE" "$FILE"  >> controls_nibe_heatpump.txt
 done <   <(find ./FHEM -maxdepth 2 \( -name "*.pm" -o -name "*.txt" \) -print0 | sort -z -g)
+
+while IFS= read -r -d '' FILE
+do
+    TIME=$(git log --pretty=format:%cd -n 1 --date=iso -- "$FILE")
+    TIME=$(TZ=Europe/Berlin date -d "$TIME" +%Y-%m-%d_%H:%M:%S)
+    FILESIZE=$(stat -c%s "$FILE")
+	FILE=$(echo "$FILE"  | cut -c 3-)
+	printf "UPD %s %-7d %s\n" "$TIME" "$FILESIZE" "$FILE"  >> controls_nibe_heatpump.txt
+done <   <(find ./www -maxdepth 2 \( -name "*.pm" -o -name "*.txt" \) -print0 | sort -z -g)
 
 # CHANGED file
 echo "FHEM NIBE_HEATPUMP last changes:" > CHANGED
